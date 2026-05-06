e9dcaa04560db855d12054732066001c9f11dbb7
Pahril <88573655+visoredkon@users.noreply.github.com>
2026-05-07 02:58:09 +0800

    build(release): bump copilot-cli 1.0.41 -> 1.0.42
    Updated release artifacts:
    - copilot-cli: 1.0.41 -> 1.0.42

--- releases/copilot-cli/release.nix
+++ releases/copilot-cli/release.nix
@@ -1,4 +1,4 @@
 {
-  sha256 = "sha256-7q10TbxGWREQzt5/B4zcoHCOzGc+Q/x1cuAdKSu13fs=";
-  version = "1.0.41";
+  sha256 = "cc8bee80f8cea693ff5b9d2c0dfd85928907c8ac4b79e2eac3e48aacd26721db";
+  version = "1.0.42";
 }
