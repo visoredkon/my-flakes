ddabe12358adf6f7c634093dcb001bf611148d99
visoredkon <88573655+visoredkon@users.noreply.github.com>
2026-05-25 04:12:30 +0000

    chore(version): bump copilot-cli 1.0.52 -> 1.0.54
    Updated release artifacts:
    - copilot-cli: 1.0.52 -> 1.0.54

--- releases/copilot-cli.nix
+++ releases/copilot-cli.nix
@@ -1,4 +1,4 @@
 {
-  sha256 = "3e32a4d1de31f2cd975d28ca0f79f4868a391478305396263216d3ffd3228f8f";
-  version = "1.0.52";
+  sha256 = "a24a665390b427c623a8b0ffd9967cde2dc0ae60126e9bde585c2bff137f4f10";
+  version = "1.0.54";
 }
