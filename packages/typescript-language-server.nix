559ab09c39ae8a1f6cf1965d6c62fb5378160c06
Pahril <88573655+visoredkon@users.noreply.github.com>
2026-06-17 04:54:17 +0800

    fix(typescript-language-server): symlink

--- packages/typescript-language-server.nix
+++ packages/typescript-language-server.nix
@@ -23,6 +23,8 @@ mkPrebuilt {
     mkdir -p "$out/lib/node_modules/typescript-language-server"
     cp -r lib package.json "$out/lib/node_modules/typescript-language-server"
 
+    ln -s ${pkgs.typescript}/lib/node_modules/typescript "$out/lib/node_modules/typescript"
+
     makeWrapper "${pkgs.nodejs}/bin/node" "$out/bin/typescript-language-server" \
       --add-flags "$out/lib/node_modules/typescript-language-server/lib/cli.mjs"
 
