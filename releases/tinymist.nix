f3ffae12a5c83c74f4d5be4e541e941817b4a1e0
Pahril <88573655+visoredkon@users.noreply.github.com>
2026-06-23 03:24:50 +0800

    chore(version): bump tinymist 0.15.0 -> 0.15.2
    Updated release artifacts:
    - tinymist: 0.15.0 -> 0.15.2

--- releases/tinymist.nix
+++ releases/tinymist.nix
@@ -1,5 +1,5 @@
 {
-  completionsSha256 = "sha256-JVm72DOapL6fLhJzwUJJoPI8w3uo7zRmRIvvfKl3vCQ=";
-  sha256 = "8e46c23b4d3047f1e9d607568057453e4f6ecfe85e95a77355c159ca3b768544";
-  version = "0.15.0";
+  completionsSha256 = "sha256-SCdkI5fSP7hydyPkT2kDOaZadUzJAXwOlhqLBg0YjXM=";
+  sha256 = "9b8a1aea6bb3fc9c39cb70496f0082bd518cfede555757bc3cb5225b05abc99b";
+  version = "0.15.2";
 }
