8476476cd08ae16d19d0777c8c47064fde6ff9a0
visoredkon <88573655+visoredkon@users.noreply.github.com>
2026-06-30 04:00:35 +0000

    chore(version): bump antigravity-cli, claude-code, warp-terminal
    Updated release artifacts:
    - antigravity-cli: 1.0.13 -> 1.0.14
    - claude-code: 2.1.195 -> 2.1.196
    - warp-terminal: 0.2026.06.24.09.19.stable_02 -> 0.2026.06.24.09.19.stable_03

--- releases/antigravity-cli.nix
+++ releases/antigravity-cli.nix
@@ -1,5 +1,5 @@
 {
-  sha256 = "6bf990458c114af3b3173dcbc1b0fb9ab93bea91c53b605fdd69aedd29a21cd9";
-  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.13-5758107482193920/linux-x64/cli_linux_x64.tar.gz";
-  version = "1.0.13";
+  sha256 = "7170d598193ee0addcaba7d8c3a2c2e307ae8622dacf749831128bd1ad3ca458";
+  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.14-6049473256882176/linux-x64/cli_linux_x64.tar.gz";
+  version = "1.0.14";
 }
--- releases/claude-code.nix
+++ releases/claude-code.nix
@@ -1,4 +1,4 @@
 {
-  sha256 = "8323e70125063147a4478b957745d835a87e5e72ffd25b838ea9a841c03e6a37";
-  version = "2.1.195";
+  sha256 = "eb933c6dd5534db89b83ba09009d5c0932bd1395f7e3bb0f34ba37eec37bbade";
+  version = "2.1.196";
 }
