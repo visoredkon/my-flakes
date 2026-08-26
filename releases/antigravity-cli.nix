898afec9a6195d0ecf3fe2cebc35d49f0919768c
Pahril <88573655+visoredkon@users.noreply.github.com>
2026-08-26 20:52:39 +0800

    chore(version): bump antigravity-cli 1.1.20 -> 1.1.21
    Updated release artifacts:
    - antigravity-cli: 1.1.20 -> 1.1.21

--- releases/antigravity-cli.nix
+++ releases/antigravity-cli.nix
@@ -1,5 +1,5 @@
 {
-  sha256 = "6ceeb0ac91df6dca60a4fa02856807ed2e2fc6d3d70bb734d1ad61a9e44ef4da";
-  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.20-5830032204103680/linux-x64/cli_linux_x64.tar.gz";
-  version = "1.1.20";
+  sha256 = "4806a347119d36be6d8ab5cc3f03319bc6aa8407a8d9203de7976a42954cabde";
+  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.21-6424454201475072/linux-x64/cli_linux_x64.tar.gz";
+  version = "1.1.21";
 }
