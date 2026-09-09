3cf0dc2f0c52f01dfe82e9d69476e0d9a11ff476
visoredkon <88573655+visoredkon@users.noreply.github.com>
2026-09-09 03:52:04 +0000

    chore(version): bump antigravity-cli, opencode
    bumped:
    - antigravity-cli: 1.1.27 -> 1.1.28
    - opencode: 1.18.29 -> 1.18.30

--- releases/antigravity-cli.nix
+++ releases/antigravity-cli.nix
@@ -1,5 +1,5 @@
 {
-  sha256 = "f874d4f6b8a73c2df660f580f25fb656fcb6e64adbfd746e6692e837fd9a20be";
-  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.27-5211191891591168/linux-x64/cli_linux_x64.tar.gz";
-  version = "1.1.27";
+  sha256 = "074ff4f732a750ad727aeed5fc82ed34b1fb72fda2a6ceba6c8e652ffd0a94b0";
+  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.28-5576113066475520/linux-x64/cli_linux_x64.tar.gz";
+  version = "1.1.28";
 }
