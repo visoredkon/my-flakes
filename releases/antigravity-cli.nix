3f47522143ae0f54dcabb96068ff096beeadd898
visoredkon <88573655+visoredkon@users.noreply.github.com>
2026-09-12 03:51:42 +0000

    chore(version): bump antigravity-cli, bootdev
    bumped:
    - antigravity-cli: 1.2.0 -> 1.2.1
    - bootdev: 1.32.2 -> 1.32.3

--- releases/antigravity-cli.nix
+++ releases/antigravity-cli.nix
@@ -1,5 +1,5 @@
 {
-  sha256 = "d9bfee1ae6e4329562cb87da1f5fc3c886d18594837e73e25c3aae00a49499b9";
-  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.2.0-5210873191596032/linux-x64/cli_linux_x64.tar.gz";
-  version = "1.2.0";
+  sha256 = "6a2c53db6c681fc114f9a1e499e7b4771357ab2852242e56acbd43197d4807f9";
+  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.2.1-5123043593420800/linux-x64/cli_linux_x64.tar.gz";
+  version = "1.2.1";
 }
--- releases/bootdev.nix
+++ releases/bootdev.nix
@@ -1,5 +1,5 @@
 {
-  sourceSha256 = "sha256-GD/VD4NCCC4ptf37T/JfKB7m8GabhtC8NCLeeX+DfkY=";
+  sourceSha256 = "sha256-6IwqKV/PWy+QMfHcDmhHvKUo9so5EECXgCrA4hg4oSs=";
   vendorHash = "sha256-ZDioEU5uPCkd+kC83cLlpgzyOsnpj2S7N+lQgsQb8uY=";
-  version = "1.32.2";
+  version = "1.32.3";
 }
