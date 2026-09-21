dfec4551c10ce26d2a69486f8e04856a321146dc
visoredkon <88573655+visoredkon@users.noreply.github.com>
2026-09-21 04:06:36 +0000

    chore(version): bump mise, waybar
    bumped:
    - mise: 2026.9.11 -> 2026.9.12
    - waybar: 1687ec7 -> 3672eee

--- releases/mise.nix
+++ releases/mise.nix
@@ -1,5 +1,5 @@
 {
-  sha256 = "02a19e4a5eda23cda916503ad09dbc608a249fe7a7d5686c5a74ff3a7ce3b7e0";
-  sourceSha256 = "f8af039b86af2046635e959ac4eff7bdfed9386cbfa70da4dae544baaf85476e";
-  version = "2026.9.11";
+  sha256 = "b4058dece685259910d3aba5782445996eea79dbdb3cf952a6eb81aadf0373ff";
+  sourceSha256 = "6d708b6c6f676a86c81f82506a4911517fe9574775cb734abd0c4c739efdca47";
+  version = "2026.9.12";
 }
--- releases/waybar.nix
+++ releases/waybar.nix
@@ -1,4 +1,4 @@
 {
-  rev = "1687ec724a89756e79371784a2ba3f9700f8e5c6";
-  sourceSha256 = "sha256-OJBVQSIpW0Ozb3czGP3+TQyDC5BBqMQtx2asPsspFHo=";
+  rev = "3672eee03a6abfe883310b92ddbcf3300ae43b2d";
+  sourceSha256 = "sha256-dc2tsgfAKqnZ/w369G6mYkfeIathdWLeyh+xgBxERU8=";
 }
