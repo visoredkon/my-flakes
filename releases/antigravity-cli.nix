b07d044fa1e9bab9b714889ebe81ee6bb70ac7c4
visoredkon <88573655+visoredkon@users.noreply.github.com>
2026-09-23 04:00:22 +0000

    chore(version): bump antigravity-cli, claude-code, copilot-cli
    bumped:
    - antigravity-cli: 1.2.7 -> 1.2.8
    - claude-code: 2.1.278 -> 2.1.280
    - copilot-cli: 1.0.87 -> 1.0.88

--- releases/antigravity-cli.nix
+++ releases/antigravity-cli.nix
@@ -1,5 +1,5 @@
 {
-  sha256 = "e410dd56d8c213ef12643d3ff5eaaab57a17e05bbf72e9415322f23879fc4a18";
-  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.2.7-6731160148115456/linux-x64/cli_linux_x64.tar.gz";
-  version = "1.2.7";
+  sha256 = "244752206d1f65c01aff489628f1df51f1a3fddacaa8ed74984661ebb6d09136";
+  url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.2.8-4907747922280448/linux-x64/cli_linux_x64.tar.gz";
+  version = "1.2.8";
 }
